25ee7994cd12bc5f728bedb5dc6e6e93b9fd1df7
Shakhzod <forworkingappsinmylaptop@gmail.com>
2022-01-02 11:44:34 +0500

    useContext and share state and function inside the app component

---
+++ src/context.js
@@ -0,0 +1,35 @@
+import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+	const [ isSidebarOpen, setIsSidebarOpen ] = useState(false);
+	const [ isModalOpen, setIsModalOpen ] = useState(false);
+
+	const openSidebar = () => {
+		setIsSidebarOpen(true);
+	};
+	const closeSidebar = () => {
+		setIsSidebarOpen(false);
+	};
+
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
+
+	return (
+		<AppContext.Provider value={{ isSidebarOpen, isModalOpen, openSidebar, closeSidebar, openModal, closeModal }}>
+			{children}
+		</AppContext.Provider>
+	);
+};
+
+// custom hook starts use keyword
+export const useGlobalContext = () => {
+	return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
